9396ea703e08ec95066971071a3fedcce6b69b44
Matt Kaes <matthew.kaes2@gmail.com>
2026-07-12 23:06:12 -0700

    [Apps::Perimortem] Canonical Window app in C++

---
+++ apps/perimortem/basic_window/main.cpp
@@ -0,0 +1,35 @@
+// Perimortem Engine
+// Copyright © Matt Kaes
+
+#include "perimortem/system/window.hpp"
+
+#include "perimortem/vulkan/renderer.hpp"
+
+using namespace Perimortem;
+
+auto main() -> int {
+  System::Window window(800, 600, "Perimortem Basic Window");
+  Vulkan::Renderer renderer(
+      window.get_display(), window.get_surface(),
+      window.get_logical_width() * window.get_scale(),
+      window.get_logical_height() * window.get_scale());
+  while (window.poll_events()) {
+    const Bits_32 width = window.get_logical_width() * window.get_scale();
+    const Bits_32 height = window.get_logical_height() * window.get_scale();
+    if (window.get_needs_resize()) {
+      renderer.resize(width, height);
+      window.clear_resize();
+    }
+
+    Vulkan::Renderer::Frame frame;
+    if (!renderer.begin_frame(frame)) {
+      renderer.resize(width, height);
+      continue;
+    }
+
+    renderer.end_frame(frame);
+  }
+
+  renderer.wait_idle();
+  return 0;
+}
