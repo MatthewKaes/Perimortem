7d6cb5879d5305d04d1dadefd0d2446506ad02d4
Matt Kaes <matthew.kaes2@gmail.com>
2026-07-29 16:20:37 -0700

    [Tetrodotoxin::Language] Dialect  supports encode / restore contract, Add `post_pass` to Monograph

---
+++ tetrodotoxin/language/dialect.cpp
@@ -0,0 +1,23 @@
+// Perimortem Engine
+// Copyright © Matt Kaes
+
+#include "tetrodotoxin/language/dialect.hpp"
+
+using namespace Tetrodotoxin;
+
+Language::Dialect::Monograph::~Monograph() {}
+
+auto Language::Dialect::Monograph::post_pass(Ttx::Lexical::Errors&) -> void {}
+
+Language::Dialect::~Dialect() {}
+
+auto Language::Dialect::encode(const Monograph&) const
+    -> Perimortem::Utility::Option<Perimortem::Memory::Dynamic::Bytes> {
+  return {};
+}
+
+auto Language::Dialect::restore(
+    Perimortem::Memory::Allocator::Arena&,
+    Perimortem::Core::View::Bytes) -> Perimortem::Utility::Option<Monograph&> {
+  return {};
+}
